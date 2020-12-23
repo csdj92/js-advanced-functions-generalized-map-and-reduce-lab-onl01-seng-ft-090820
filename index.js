4bd01d68d91d81531389e5b54bcf8157de12cb53
csdj92 <csdj92@gmail.com>
2020-12-23 11:27:13 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Add your functions here
+
+function map(array, fnctn){
+    return array.map(fnctn)
+}
+
+function reduce(array, cllbck, startValue){
+    if (!!startValue === false){
+        startValue = array.shift()
+    }
+    return array.reduce(cllbck, startValue)
+}
